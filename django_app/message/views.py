dd27206d0c854b59a50b9375582a344fa9df09cf
Gihong Kim <gyong1211@gmail.com>
2017-06-27 22:31:32 +0900

    sms_send view에 POST 요청이 온 경우 처리할 구문 추가, utils에 coolsms API를 사용한 send_message 추가

--- django_app/message/views.py
+++ django_app/message/views.py
@@ -1,14 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from utils import message
+from .models import Message
 from .forms import MessageForm
+from secret_key import keys
 
 
 def sms_send(request):
     if request.method == 'POST':
-        pass
+        form = MessageForm(data=request.POST)
+        if form.is_valid():
+            recipient = form.cleaned_data['recipient']
+            content = form.cleaned_data['content']
+            results = message.send_message(recipient, content)
+            Message.objects.create(
+                sender=keys.api_phone_number,
+                recipient=recipient,
+                content=content
+            )
+            context = {
+                'form': MessageForm(),
+                'results': results
+            }
     else:
-        form = MessageForm()
         context = {
-            'form': form,
+            'form': MessageForm(),
         }
-        return render(request, 'message/send.html', context=context)
+    return render(request, 'message/send.html', context=context)
---
+++ django_app/utils/message/__init__.py
@@ -0,0 +1,35 @@
+from secret_key import keys
+
+from sdk.api.message import Message
+from sdk.exceptions import CoolsmsException
+
+
+def send_message(number, text):
+    api_key = keys.api_key
+    api_secret = keys.api_secret
+
+    params = dict()
+    params['type'] = 'sms'  # Message type ( sms, lms, mms, ata )
+    params['to'] = number  # Recipients Number '01000000000,01000000001'
+    params['from'] = keys.api_phone_number  # Sender number
+    params['text'] = text  # Message
+
+    cool = Message(api_key, api_secret)
+    try:
+        response = cool.send(params)
+        result = [
+            "Sender : {}".format(params['from']),
+            "Recipients : {}".format(params['to']),
+            "Success Count : {}".format(response['success_count']),
+            "Group ID : {}".format(response['group_id']),
+            "Error Count : {}".format(response['error_count'])
+        ]
+
+        if "error_list" in response:
+            result.append("Error List : {}".format(response['error_list']))
+        return result
+    except CoolsmsException as e:
+        error_code = e.code
+        error_message = e.msg
+        result = ["Error Code : {}".format(error_code), "Error Message : {}".format(error_message)]
+        return result
